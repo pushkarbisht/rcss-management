6e4563482fa668f7b6d06476f02351f8f3f44712
pushkar singh <er.pushkarbisht@gmail.com>
2021-09-27 18:05:58 +0530

    member static table

--- src/app/dashboard/members-list/members-list.component.ts
+++ src/app/dashboard/members-list/members-list.component.ts
@@ -2,23 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 
 export interface PeriodicElement {
+  id:string
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  designation: string;
+  email: string;
+  contact: string;
+  colliery:string;
+
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {id: "1", name: 'Pushkar Singh', designation:"Engineer", email: 'er.pushkarbisht@gmail.com',contact:"1234567890",colliery:"Singrauli"},
+  {id: "2", name: 'Pushkar Singh', designation:"Engineer", email: 'er.pushkarbisht@gmail.com',contact:"1234567890",colliery:"Singrauli"},
+  {id: "3", name: 'Pushkar Singh', designation:"Engineer", email: 'er.pushkarbisht@gmail.com',contact:"1234567890",colliery:"Singrauli"},
+  {id: "4", name: 'Pushkar Singh', designation:"Engineer", email: 'er.pushkarbisht@gmail.com',contact:"1234567890",colliery:"Singrauli"},
+  {id: "5", name: 'Pushkar Singh', designation:"Engineer", email: 'er.pushkarbisht@gmail.com',contact:"1234567890",colliery:"Singrauli"},
+  {id: "6", name: 'Pushkar Singh', designation:"Engineer", email: 'er.pushkarbisht@gmail.com',contact:"1234567890",colliery:"Singrauli"},
 ];
 
 
@@ -28,7 +27,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./members-list.component.css']
 })
 export class MembersListComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['ID', 'NAME', 'DESIGNATION', 'EMAIL','CONTACT','COLLIERY'];
   dataSource = ELEMENT_DATA;
   constructor() { }
 
