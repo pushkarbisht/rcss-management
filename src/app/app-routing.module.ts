b969ba2b88844ad3211fbc8a36e8624dde8e32c4
Pushkar Singh Bisht <31041986+pushkarbisht@users.noreply.github.com>
2021-09-27 14:53:49 +0530

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './dashboard/accounts/accounts.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { KpisComponent } from './dashboard/kpis/kpis.component';
+import { MembersListComponent } from './dashboard/members-list/members-list.component';
+import { SettingsComponent } from './dashboard/settings/settings.component';
+import { UploadComponent } from './dashboard/upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:DashboardComponent,
+    children:[
+      {
+        path:'',
+        component:KpisComponent
+      },
+      {
+        path:'members',
+        component:MembersListComponent
+      },
+      {
+        path:'accounts',
+        component:AccountsComponent
+      },
+      {
+        path:'settings',
+        component:SettingsComponent
+      },
+      {
+        path:'upload',
+        component:UploadComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,59 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+import {MatSortModule} from '@angular/material/sort';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MembersListComponent } from './dashboard/members-list/members-list.component';
+import {MatListModule} from '@angular/material/list';
+import { KpisComponent } from './dashboard/kpis/kpis.component';
+import { AccountsComponent } from './dashboard/accounts/accounts.component';
+import { SettingsComponent } from './dashboard/settings/settings.component';
+import { UploadComponent } from './dashboard/upload/upload.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MembersListComponent,
+    KpisComponent,
+    AccountsComponent,
+    SettingsComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
